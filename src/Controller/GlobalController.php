7426ff833011cc670d4375867829a8f3857d35f5
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-16 21:05:54 +0100

    La quatrieme application, 9-Gestion des utilisateurs

--- src/Controller/GlobalController.php
+++ src/Controller/GlobalController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Utilisateur;
+use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class GlobalController extends AbstractController
 {
@@ -16,4 +21,27 @@ class GlobalController extends AbstractController
         return $this->render('global/accueil.html.twig');
     }
 
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function inscription(Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder): Response
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(InscriptionType::class,$utilisateur);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $passwordCrypte = $encoder->encodePassword($utilisateur,$utilisateur->getPassword());
+            $utilisateur->setPassword($passwordCrypte);
+            $utilisateur->setRoles("ROLE_USER");
+            $entityManager->persist($utilisateur);
+            $entityManager->flush();
+            return $this->redirectToRoute("accueil");
+        }
+
+        return $this->render('global/inscription.html.twig',[
+            "form" => $form->createView()
+        ]);
+    }
+    
 }
---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UtilisateurRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=UtilisateurRepository::class)
+ * @UniqueEntity(
+ * fields={"username"},
+ * message="Le nom d'utilisateur existe déjà"
+ * )
+ */
+class Utilisateur implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min = 5, max = 10, minMessage = "Le nom d'utilisateur doit avoir plus de 5 caractères",maxMessage = "Le nom d'utilisateur doit avoir moins de 10 caractères")
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min = 5, max = 10, minMessage = "Il faut plus de 5 caractères",maxMessage = "Il faut moins de 10 caractères")
+     */
+    private $password;
+
+    /**
+     * @Assert\Length(min = 5, max = 10, minMessage = "Il faut plus de 5 caractères",maxMessage = "Il faut moins de 10 caractères")
+     * @Assert\EqualTo(propertyPath = "password", message = "Les mots de passes ne sont pas équivalents")
+     */
+    private $verificationPassword;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $roles;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getVerificationPassword(): ?string
+    {
+        return $this->verificationPassword;
+    }
+
+    public function setVerificationPassword(string $verificationPassword): self
+    {
+        $this->verificationPassword = $verificationPassword;
+
+        return $this;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt(){
+        
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials(){
+
+    }
+    
+    public function getRoles(): ?string
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(string $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+}
