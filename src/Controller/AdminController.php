695f5d47f6512dce8f5ebc2c94ae36f5831707ba
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-16 17:00:10 +0100

    La quatrieme application, 7-CRUD-Etape 1

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -11,36 +11,27 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class VoitureController extends AbstractController
+class AdminController extends AbstractController
 {
     /**
-     * @Route("/client/voitures", name="voitures")
+     * @Route("/admin", name="admin")
      */
-    public function index(VoitureRepository $repository, PaginatorInterface $paginatorInterface, Request $request): Response
+    public function index(VoitureRepository $repo,PaginatorInterface $paginatorInterface, Request $request): Response
     {
         $rechercheVoiture = new RechercheVoiture();
 
-        $form = $this->createForm(RechercheVoitureType::class, $rechercheVoiture);
+        $form = $this->createForm(RechercheVoitureType::class,$rechercheVoiture);
         $form->handleRequest($request);
 
         $voitures = $paginatorInterface->paginate(
-            $repository->findAllWithPagination($rechercheVoiture),
+            $repo->findAllWithPagination($rechercheVoiture),
             $request->query->getInt('page', 1), /*page number*/
             6 /*limit per page*/
         );
-
-        $voitures->setCustomParameters([
-            'align' => 'center', # center|right (for template: twitter_bootstrap_v4_pagination and foundation_v6_pagination)
-            'size' => 'medium', # small|large (for template: twitter_bootstrap_v4_pagination)
-            'style' => 'bottom',
-            'span_class' => 'whatever',
-        ]);
-
         return $this->render('voiture/voitures.html.twig',[
             "voitures" => $voitures,
             "form" => $form->createView(),
-            "admin" => false
+            "admin" => true
         ]);
-
     }
 }
